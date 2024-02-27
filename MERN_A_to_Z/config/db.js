84269f6674c53a40629fd7ebd86fc9d5972d3b6f
Your Name <you@example.com>
2024-02-28 00:55:35 +0100

    .

---
+++ MERN_A_to_Z/config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const db =
+  "mongodb+srv://MERN_A_to_Z:fruergaard85@cluster0.afnwd7o.mongodb.net/";
+/* Replace <password> with your database password */
+
+mongoose.set("strictQuery", true, "useNewUrlParser", true);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB is Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
